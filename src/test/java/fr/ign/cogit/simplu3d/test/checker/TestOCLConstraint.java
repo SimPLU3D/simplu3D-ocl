af7287c105a9a8fb8948e4515c1767be2b379405
Mickaël Brasebin <mickael.brasebin@ign.fr>
2015-10-09 18:36:34 +0200

    - Minor modification

--- src/test/java/fr/ign/cogit/simplu3d/test/checker/TestOCLConstraint.java
+++ src/test/java/fr/ign/cogit/simplu3d/test/checker/TestOCLConstraint.java
@@ -1,6 +1,6 @@
 package fr.ign.cogit.simplu3d.test.checker;
 
-import java.io.FileNotFoundException;
+
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -8,10 +8,8 @@ import org.junit.Test;
 import tudresden.ocl20.pivot.modelinstancetype.types.IModelInstanceObject;
 import fr.ign.cogit.simplu3d.application.model.EnvironnementOCL;
 import fr.ign.cogit.simplu3d.checker.ExhaustiveChecker;
-import fr.ign.cogit.simplu3d.exe.LoadDefaultEnvironment;
 import fr.ign.cogit.simplu3d.importer.ocl.LoaderSHPOCL;
 import fr.ign.cogit.simplu3d.model.application.BasicPropertyUnit;
-import fr.ign.cogit.simplu3d.model.application.Environnement;
 import junit.framework.Assert;
 
 public class TestOCLConstraint {
