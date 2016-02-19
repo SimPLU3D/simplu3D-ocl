74823cc78fea29a337e85a31cca6709d637f4563
Mickael Brasebin <mickael.brasebin@ign.fr>
2016-02-19 14:30:14 +0100

    Changement of file name is necessary since there are some changes in simplu3d-rules configuration

--- src/test/java/fr/ign/cogit/simplu3d/test/checker/TestOCLConstraint.java
+++ src/test/java/fr/ign/cogit/simplu3d/test/checker/TestOCLConstraint.java
@@ -9,6 +9,7 @@ import tudresden.ocl20.pivot.modelinstancetype.types.IModelInstanceObject;
 import fr.ign.cogit.simplu3d.application.model.EnvironnementOCL;
 import fr.ign.cogit.simplu3d.checker.ExhaustiveChecker;
 import fr.ign.cogit.simplu3d.importer.ocl.LoaderSHPOCL;
+import fr.ign.cogit.simplu3d.io.load.application.LoaderSHP;
 import fr.ign.cogit.simplu3d.model.application.BasicPropertyUnit;
 import junit.framework.Assert;
 
@@ -16,13 +17,26 @@ public class TestOCLConstraint {
 
   @Test
   public void testInterpretation() throws Exception {
+	  
+	    
+	    // Rerouting towards the new files
+	    LoaderSHP.NOM_FICHIER_PLU = "DOC_URBA.shp";
+	    LoaderSHP.NOM_FICHIER_ZONAGE = "ZONE_URBA.shp";
+	    LoaderSHP.NOM_FICHIER_PARCELLE = "parcelle.shp";
+	    LoaderSHP.NOM_FICHIER_TERRAIN = "MNT_BD3D.asc";
+	    LoaderSHP.NOM_FICHIER_VOIRIE = "route.shp";
+	    
     EnvironnementOCL env = LoaderSHPOCL.load("src/test/resources/fr/ign/cogit/simplu3d/data/");
+    
+
+    
+
 
     for (BasicPropertyUnit bPU : env.getBpU()) {
       ExhaustiveChecker vFR = new ExhaustiveChecker(bPU, env.getUrbaZoneOCL().get(0));
 
       vFR.check(new ArrayList<IModelInstanceObject>());
-
+      
     }
     Assert.assertEquals(true, true);
   }
