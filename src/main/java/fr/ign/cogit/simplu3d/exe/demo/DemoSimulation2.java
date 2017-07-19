48de003ee0122fc5c56cc3c49808808a0fd8444a
Mickael Brasebin <mickael.brasebin@ign.fr>
2017-07-19 09:37:32 +0200

    Update

--- src/main/java/fr/ign/cogit/simplu3d/exe/demo/DemoSimulation2.java
+++ src/main/java/fr/ign/cogit/simplu3d/exe/demo/DemoSimulation2.java
@@ -5,16 +5,11 @@ import java.io.File;
 import fr.ign.cogit.geoxygene.api.feature.IFeature;
 import fr.ign.cogit.geoxygene.api.feature.IFeatureCollection;
 import fr.ign.cogit.geoxygene.feature.FT_FeatureCollection;
-import fr.ign.cogit.geoxygene.sig3d.representation.basic.Object2d;
 import fr.ign.cogit.geoxygene.util.conversion.ShapefileWriter;
 import fr.ign.cogit.simplu3d.experiments.thesis.predicate.UB16PredicateWithOtherBuildings;
-import fr.ign.cogit.simplu3d.experiments.thesis.predicate.UXL3PredicateBuildingSeparation;
-import fr.ign.cogit.simplu3d.importer.ocl.LoaderSHPOCL;
 import fr.ign.cogit.simplu3d.io.nonStructDatabase.shp.LoaderSHP;
 import fr.ign.cogit.simplu3d.model.BasicPropertyUnit;
 import fr.ign.cogit.simplu3d.model.Environnement;
-import fr.ign.cogit.simplu3d.model.EnvironnementOCL;
-import fr.ign.cogit.simplu3d.rjmcmc.cuboid.geometry.impl.Cuboid;
 import fr.ign.cogit.simplu3d.rjmcmc.paramshp.geometry.impl.LBuildingWithRoof;
 import fr.ign.cogit.simplu3d.rjmcmc.paramshp.optimizer.OptimisedLShapeDirectRejection;
 import fr.ign.cogit.simplu3d.util.AssignZ;
@@ -47,7 +42,6 @@ public class DemoSimulation2 {
 		// the section 4 of the article)
 		AssignZ.DEFAULT_Z = 139;
 		Environnement env = LoaderSHP.load(new File(folderIn));
-		
 
 		// File that determines parameters for the simulator (inputs of the
 		// optimization algorithm - section 5 of the article)
@@ -56,14 +50,11 @@ public class DemoSimulation2 {
 
 		// Writing the output
 		IFeatureCollection<IFeature> iFeatC = new FT_FeatureCollection<>();
-		
-		
+
 		int nbParcels = env.getBpU().size();
 
-		for (int i= 0; i < nbParcels;i++) {
-			
-		
-			
+		for (int i = 0; i < nbParcels; i++) {
+
 			BasicPropertyUnit bpu = env.getBpU().get(i);
 
 			// Definition of the predicate that checks the rule with the chosen
@@ -71,7 +62,7 @@ public class DemoSimulation2 {
 
 			UB16PredicateWithOtherBuildings<LBuildingWithRoof, GraphConfiguration<LBuildingWithRoof>, BirthDeathModification<LBuildingWithRoof>> pred3 = null;
 			pred3 = new UB16PredicateWithOtherBuildings<>(bpu, 5, 2);
-			
+
 			// L'optimizer
 			OptimisedLShapeDirectRejection optimizer = new OptimisedLShapeDirectRejection();
 			GraphConfiguration<LBuildingWithRoof> cc = optimizer.process(Random.random(), bpu, p, env, bpu.getId(),
